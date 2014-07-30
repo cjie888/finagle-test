27bbf2cb24db27ea5b5994e9d8397533408f9294
huchengjie <huchengjie@dianxinos.com>
2014-07-30 19:55:17 +0800

    option demo

---
+++ src/main/scala/cn/chinahoop/scala/OptionDemo.scala
@@ -0,0 +1,21 @@
+package cn.chinahoop.scala
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hucj
+ * Date: 14-7-30
+ * Time: 下午7:39
+ * To change this template use File | Settings | File Templates.
+ */
+//Option 包括Some和None
+object OptionDemo extends App {
+  val m = Map(1->2)
+  m.get(1) match {
+    case Some(v) => println(v)
+    case None => println("no such key")
+  }
+
+  val s ="Yes"
+  val b = s"$s,I do"
+  println(b)
+}
