bcff6ab6b3e90d33948cafd21eaf97b09c7d9ff0
cjie888 <cjie888163.com>
2014-08-02 15:00:52 +0800

    spark-streaming

---
+++ src/main/scala/cn/chinahoop/streaming/NetworkWordCount.scala
@@ -0,0 +1,28 @@
+package cn.chinahadoop.streaming
+
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.storage.StorageLevel
+
+/**
+ * Created by Administrator on 14-7-27.
+ */
+object NetworkWordCount {
+   def main(args:Array[String]) {
+     if (args.length < 4) {
+       System.err.println("Usage: NetworkWordCount <master> <hostname> <port> <seconds>\n"
+         +"In local mode,<master> should be 'local[n]' with n>1")
+       System.exit(1)
+     }
+     val ssc = new StreamingContext(args(0),"NetWorkCount",Seconds(args(3).toInt),
+       System.getenv("SPARK_HOME"), StreamingContext.jarOfClass(this.getClass))
+     val lines = ssc.socketTextStream(args(1),args(2).toInt,StorageLevel.MEMORY_ONLY_SER)
+     val words = lines.flatMap(_.split(" "))
+     // val wordCounts = words.map(x => (x,1)).reduceByKey(_+_)
+     val wordCounts = words.map(x => (x,1)).reduceByKey(_+_).reduce((a, b) => if (a._2 > b._2) a else b)
+     wordCounts.print()
+     ssc.start()
+     ssc.awaitTermination()
+   }
+}
---
+++ src/main/scala/cn/chinahoop/streaming/WindowCount.scala
@@ -0,0 +1,31 @@
+package streaming
+
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.storage.StorageLevel
+
+/**
+ * Created by Administrator on 14-7-27.
+ */
+object WindowCount {
+  def main(args:Array[String]) {
+    if (args.length < 4) {
+      System.err.println("Usage: windowCount <master> <hostname> <port> <interval>\n"
+        + "<windowLength> <slideinterval>"
+        +"In local mode,<master> should be 'local[n]' with n>1")
+      System.exit(1)
+    }
+    val ssc = new StreamingContext(args(0),"WindowCounter",Seconds(args(3).toInt),
+      System.getenv("SPARK_HOME"), StreamingContext.jarOfClass(this.getClass))
+    ssc.checkpoint(".")
+    val lines = ssc.socketTextStream(args(1),args(2).toInt,StorageLevel.MEMORY_ONLY_SER)
+    val words = lines.flatMap(_.split(" "))
+    //val wordCounts = words.map(x=>(x,1)).reduceByWindow((x:Int,y:Int)=>x+y,Seconds(args(4)),Seconds(args(5)))
+    val wordCounts = words.map(x=>(x,1)).reduceByKeyAndWindow(_+_,_-_,Seconds(args(4).toInt),Seconds(args(5).toInt)).reduce((a, b) => if (a._2 > b._2) a else b)
+
+    wordCounts.print()
+    ssc.start
+    ssc.awaitTermination()
+
+  }
+}
