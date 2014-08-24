0b6651aad135c3e89bb48ae48a1b9d42e8a95a87
cjie888 <cjie888@163.com>
2014-08-24 22:14:48 +0800

    sql parquet

---
+++ src/main/scala/cn/cjie/sql/SQLParquet.scala
@@ -0,0 +1,32 @@
+package cn.cjie.sql
+
+import org.apache.spark.SparkContext
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-8-24
+ * Time: 下午10:01
+ * To change this template use File | Settings | File Templates.
+ */
+object SQLParquet extends App{
+  val sc: SparkContext = new SparkContext("local[2]","graphx")
+  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+  import sqlContext._
+
+  val data = sc.textFile("data/test/people")
+
+  case class People(name:String, age:Int)
+
+  val people = data.map(_.split(',')).map(p=>People(p(0),p(1).toInt))
+
+  people.saveAsParquetFile("data/test/people/parquet")
+
+  val parquetFile = sqlContext.parquetFile("data/test/people/parquet")
+
+  parquetFile.registerAsTable("parquetFile")
+
+  val rs = sql("select name from parquetFile where age")
+  rs.map(r=>"name:"+r(0)).collect().foreach(println)
+}
