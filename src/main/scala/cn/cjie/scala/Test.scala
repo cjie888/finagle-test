4088848eac171414437897a0f6b0dc8bf6663fb4
huchengjie <huchengjie@dianxinos.com>
2014-10-29 19:13:17 +0800

    java collection to scala seq

--- src/main/scala/cn/cjie/scala/Test.scala
+++ src/main/scala/cn/cjie/scala/Test.scala
@@ -7,6 +7,17 @@ package cn.cjie.scala
  * Time: 下午6:40
  * To change this template use File | Settings | File Templates.
  */
+
+import scala.collection.JavaConversions._
+import java.util.Collection
+import org.apache.spark.SparkContext
+
 object Test extends  App {
   println("Hello")
+  val list:Collection[String] = new java.util.ArrayList[String]()
+  list.add("test")
+  list.add("test2")
+  val set = list.toSeq
+  val sc = new SparkContext("local[2]","test")
+  sc.parallelize(set).collect().foreach(println)
 }
